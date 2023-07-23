ee214b12a4a0a8940441d4d06d7b21df88df4a83
ue-an <fujisakiian@gmail.com>
2023-07-23 20:43:50 +0800

    removed sensitive files/information

--- lib/models/user_account.dart
+++ lib/models/user_account.dart
@@ -1,16 +1,1 @@
-class UserAccount {
-  String email;
-  String studentNumber;
-
-  UserAccount({
-    this.email,
-    this.studentNumber,
-  });
-
-  factory UserAccount.fromJson(Map<String, dynamic> json) {
-    return UserAccount(
-      email: json['email'],
-      studentNumber: json['student_number'],
-    );
-  }
-}
+//removed sensitive information/data
